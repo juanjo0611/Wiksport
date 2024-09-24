47d56569c26dbc011670082ba6b8b189e12c1f56
juanjo0611 <133078794+juanjo0611@users.noreply.github.com>
2024-09-23 21:15:07 -0500

    Delete Model/indices_protecto_estructuras.sql

--- Model/indices_protecto_estructuras.sql
+++
@@ -1,5 +0,0 @@
-use proyecto_estructuras;
-#drop index  idx_ejercicio_categoria on ejercicio;
-create index idx_ejercicio_categoria ON ejercicio(Categoria);
-#drop index idx_ejercicio_nombre_ejercicio on ejercicio;
-create index idx_ejercicio_nombre_ejercicio on ejercicio(Nombre_ejercicio);
